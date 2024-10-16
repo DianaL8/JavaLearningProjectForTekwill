27302310bf21d1771f7edbe24e0d04334c171573
DianaL8 <lozovanu.diana8@gmail.com>
2024-10-16 16:30:19 +0300

    Added the Variables and Comments Tasks

---
+++ src/main/java/com/dianaqa/variableandcomments/VariablesAndComments.java
@@ -0,0 +1,31 @@
+package com.dianaqa.variableandcomments;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+
+        // Declare section
+
+        boolean exampleOfBooleanVariable;
+        char exampleOfCharVariable;
+        int exampleOfIntVariable;
+        byte exampleOfByteVariable;
+        short exampleOfShortVariable;
+        long exampleOfLongVariable;
+        float exampleOfFloatVariable;
+        double exampleOfDoubleVariable;
+
+        // Intialization section
+
+        exampleOfCharVariable = 'D';
+        exampleOfIntVariable = 123456;
+        exampleOfByteVariable = 17;
+        exampleOfShortVariable = 45000;
+        exampleOfLongVariable = 5689765;
+        exampleOfFloatVariable = 36,8f;
+        exampleOfDoubleVariable = 345.876544;
+
+        System.out.printIn(exampleOfShortVariable);
+    }
+
+}
